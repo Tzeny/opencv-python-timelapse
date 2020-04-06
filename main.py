f5d10d80ea5ee28ae898d04cc80188558b4a336e
Andrei Tenescu <teny96@gmail.com>
2020-04-06 21:39:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import traceback
+
+import cv2
+
+done = False
+
+while not done:
+    try:
+        cap = cv2.VideoCapture("rtsp://tzeny:ghtwaylat@192.168.0.108/live")
+
+        output_path = './output/'
+
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            
+            datetime = datetime.now().strftime("%d.%m.%Y-%H:%M:%S")
+
+            cv2.imwrite(output_path + datetime + '.jpg', frame)
+
+            break
+
+        cap.release()
+        done = True
+    except:
+        print('Error capturing frame at ' + datetime.now().strftime("%d.%m.%Y-%H:%M:%S"))
+        traceback.print_exc()
